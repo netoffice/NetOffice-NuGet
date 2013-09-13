b6bb6df5df680b93ffad2bc739bfb506f6a213a0
Erika LeBlanc <erika.leblanc88@outlook.com>
2013-09-13 20:08:28 -0300

    Add NetOffice.Excel NuGet Package v1.6.0.0 setup

--- nuget/NuGetPack.cmd
+++ nuget/NuGetPack.cmd
@@ -5,4 +5,11 @@ nuget pack NetOffice.Core\NetOffice.Core.Net35.nuspec
 nuget pack NetOffice.Core\NetOffice.Core.Net40.nuspec
 nuget pack NetOffice.Core\NetOffice.Core.Net45.nuspec
 
+nuget pack NetOffice.Excel\NetOffice.Excel.nuspec
+nuget pack NetOffice.Excel\NetOffice.Excel.Net20.nuspec
+nuget pack NetOffice.Excel\NetOffice.Excel.Net30.nuspec
+nuget pack NetOffice.Excel\NetOffice.Excel.Net35.nuspec
+nuget pack NetOffice.Excel\NetOffice.Excel.Net40.nuspec
+nuget pack NetOffice.Excel\NetOffice.Excel.Net45.nuspec
+
 pause
