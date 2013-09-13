acdc621e1227ef047b18d84628ed5f26f562f9ce
Erika LeBlanc <erika.leblanc88@outlook.com>
2013-09-13 20:07:55 -0300

    Add NetOffice.Core NuGet Package v1.6.0.0 setup

---
+++ nuget/NuGetPack.cmd
@@ -0,0 +1,8 @@
+nuget pack NetOffice.Core\NetOffice.Core.nuspec
+nuget pack NetOffice.Core\NetOffice.Core.Net20.nuspec
+nuget pack NetOffice.Core\NetOffice.Core.Net30.nuspec
+nuget pack NetOffice.Core\NetOffice.Core.Net35.nuspec
+nuget pack NetOffice.Core\NetOffice.Core.Net40.nuspec
+nuget pack NetOffice.Core\NetOffice.Core.Net45.nuspec
+
+pause
